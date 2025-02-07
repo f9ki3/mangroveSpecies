34a44c18f60360d62165c7335723d3e3c53700e2
Fyke Lleva <110708999+f9ki3@users.noreply.github.com>
2025-02-08 02:39:23 +0800

    updated structure with albert

--- app.py
+++ app.py
@@ -2,10 +2,22 @@ from flask import Flask, render_template
 
 app =Flask(__name__)
 
+#pages route
 @app.route('/')
 def home():
     return render_template('pages/index.html')
 
+@app.route('/image_detect')
+def image_detect():
+    return render_template('pages/image_detect.html')
+
+@app.route('/about')
+def about():
+    return render_template('pages/about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('pages/contact.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
